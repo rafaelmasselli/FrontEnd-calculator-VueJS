ff148b87760f1ff2f4b325bef27f9cdaa253af7d
rafaelmasselli <rafaelmasselli0@gmail.com>
2022-11-30 21:35:32 -0300

    feat: project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from "./pages/home.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Calculator,
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: "robotoMono" monospace;
+}
+
+body {
+  margin: 0;
+  color: white;
+  background: rgb(80, 78, 78);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
